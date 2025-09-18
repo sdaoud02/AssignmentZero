f347586d228e3cbf56c483fe850f5e7c0282bdb4
drev <dtaca943@mtroyal.ca>
2025-09-18 14:08:47 -0600

    done1

--- AssingmentZero/src/app/WorkSpace.java
+++ AssingmentZero/src/app/WorkSpace.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WorkSpace {
@@ -19,6 +20,44 @@ public class WorkSpace {
         keyboard.close();
 
     }
+
+    	//This calculates an applicant's score based on their age.
+	public static void calculateScoreAge(ArrayList<String> applicantList) {
+		for (int i = 0; i < applicantList.size(); i++) {
+			int age = Integer.parseInt(applicantList.get(i));
+			int score = 0;
+			
+			if (age < 18) {
+				score += 0;
+			} else if (age >= 18 && age <= 35) {
+				score += 12;
+			} else if (age == 36) {
+				score += 11;
+			} else if (age == 37) {
+				score += 10;
+			} else if (age == 38) {
+				score += 9;
+			} else if (age == 39) {
+				score += 8;
+			} else if (age == 40) {
+				score += 7;
+			} else if (age == 41) {
+				score += 6;
+			} else if (age == 42) {
+				score += 5;
+			} else if (age == 43) {
+				score += 4;
+			} else if (age == 44) {
+				score += 3; 
+			} else if (age == 45) {
+				score += 2;
+			} else if (age == 46) {
+				score += 1;
+			} else if (age >= 47) {
+				score += 0;
+			}
+
+
     private static int applicantFiles(String inputFileName, String outputFileName) {
 		return ;
 	}
