3cc07259736e111a235ea1361928a58fffed986e
sdaoud02 <Sdaou829@mtroyal.ca>
2025-09-18 13:15:54 -0600

    commit

--- AssingmentZero/src/app/WorkSpace.java
+++ AssingmentZero/src/app/WorkSpace.java
@@ -1,11 +1,26 @@
-package app;
 
 import java.util.Scanner;
 
 public class WorkSpace {
 	
-	
+    public static void main (String [] args) {
+    	
+    	Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please provide the name of the input file (to be located in data/input/): ");
+        String inputFileName = keyboard.nextLine();
+        
+        System.out.print("Please provide the name of the output file (to be located in data/input/): ");
+        String outputFileName = keyboard.nextLine();
+       
+        int qualifiedCount = applicantFiles(inputFileName, outputFileName);
+    
+        System.out.println("There were qualified applicants: " + qualifiedCount);
+        
+        keyboard.close();
+
+    }
+    private static int applicantFiles(String inputFileName, String outputFileName) {
+		return ;
+	}
+
 }
-
-
-
