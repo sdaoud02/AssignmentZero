de25044ca8e6f1a0f31bae4cd372b9bf4b636404
sdaoud02 <Sdaou829@mtroyal.ca>
2025-09-23 14:12:47 -0600

    updated new code

--- JustDoit.java
+++ JustDoit.java
@@ -2,6 +2,7 @@
 package app;
 
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -9,30 +10,9 @@ import java.util.Scanner;
 
 public class JustDoit {
 
-    // all private constants which will be used later on in the code
-    private static final int INDEX_FIRST_NAME = 0;
-    private static final int INDEX_LAST_NAME = 1;
-    private static final int INDEX_AGE = 2;
-    private static final int INDEX_READING = 4;
-    private static final int INDEX_WRITING = 5;
-    private static final int INDEX_SPEAKING = 6;
-    private static final int INDEX_LISTENING = 7;
-    private static final int INDEX_SPOUSE_LANGUAGE = 8;
-    private static final int INDEX_EDUCATION = 9;
-    private static final int INDEX_EXPERIENCE = 10;
-    private static final int INDEX_ARRANGED_EMPLOYMENT = 11;
-    private static final int INDEX_ADAPTABILITY_1 = 12;
-    private static final int INDEX_ADAPTABILITY_2 = 13;
-    private static final int INDEX_ADAPTABILITY_3 = 14;
-    private static final int INDEX_ADAPTABILITY_4 = 15;
-    private static final int INDEX_ADAPTABILITY_5 = 16;
-    private static final int INDEX_ADAPTABILITY_6 = 17;
-    private static final int INDEX_ADAPTABILITY_7 = 18;
-    private static final int MIN_REQUIRED_FIELDS = 19;
-    
-    
     public static void main(String[] args) throws FileNotFoundException {
         Scanner input = new Scanner(System.in);
+        
 		// create a switch case that reads the dataset and if the case is "secondary school(high school diploma)" or other education return the appropriate score
 			// find a way to read the item right after the education and based on what it says return a score based on the scoring criteria again with a switch case
 		//System.out.println("Enter your name: ");
@@ -52,13 +32,20 @@ public class JustDoit {
             fileScanner.nextLine();
         }
 
-        // will count the applicants who are qualified and will collect their data 
-        ArrayList<String> qualifiedRows = new ArrayList<>();
+        // will process file and write the output
+        PrintWriter outputFile = new PrintWriter("data/output/" + outputFileName);
         int qualifiedCount = 0;
+
+        
+        outputFile.println("First name       | Last name       | Age | Score");
+        outputFile.println("-----------------+-----------------+-----+------");
+        System.out.println("First name       | Last name       | Age | Score");
+        System.out.println("-----------------+-----------------+-----+------");
+
 
         while (fileScanner.hasNextLine()) {
             String line = fileScanner.nextLine();
-            String[] applicantData = line.split("\t");
+            String[] applicantData = line.split("\t"); 
 
             if (applicantData.length >= 19) {
                 String firstName = applicantData[0];
@@ -69,58 +56,40 @@ public class JustDoit {
 
                 if (points >= 67) {
                     qualifiedCount++;
-                    String row = firstName + " ".repeat(16 - firstName.length()) + " | "
-                           + lastName + " ".repeat(15 - lastName.length()) + " | "
-                           + String.format("%3d | %3d", age, points);
-                    qualifiedRows.add(row); // Store for later output
+                    
+                 // Fixed the variable name and printf formatting
+                System.out.printf("%-16s | %-15s | %3d | %3d\n", firstName, lastName, age, points);
+                 outputFile.printf("%-16s | %-15s | %3d | %3d\n", firstName, lastName, age, points);
                 }
             }
         }
-        fileScanner.close();
-
-        // will write the output file
-        PrintWriter outputFile = new PrintWriter("data/output/" + outputFileName);
-        
-        // Output qualified count first
-        String countMessage = "There were " + qualifiedCount + " qualified applicants";
-        System.out.println(countMessage);
-        outputFile.println(countMessage);
-        outputFile.println();    
-        
-        // Output table headers
-        outputFile.println("First name       | Last name       | Age | Score");
-        outputFile.println("-----------------+-----------------+-----+------");
-        System.out.println("\nFirst name       | Last name       | Age | Score");
-        System.out.println("-----------------+-----------------+-----+------");
-        
-        // will write an output of all the applicants who are qualified
-        for (String row : qualifiedRows) {
-            outputFile.println(row);
-            System.out.println(row);
-        }
 
         outputFile.close();
+        fileScanner.close();
         input.close();
+
+        System.out.println("\nThere were " + qualifiedCount + " qualified applicants");
     }
+
 
     public static int calculateTotalScore(String[] applicantData) {
         int totalScore = 0;
         totalScore += calculateLanguagePoints(applicantData);
-        totalScore += calculateEducationPoints(applicantData[INDEX_EDUCATION]);
-        totalScore += calculateWorkExperiencePoints(applicantData[INDEX_EXPERIENCE]);
-        totalScore += calculateAgePoints(Integer.parseInt(applicantData[INDEX_AGE]));
-        totalScore += calculateArrangedEmploymentPoints(applicantData[INDEX_ARRANGED_EMPLOYMENT]);
+        totalScore += calculateEducationPoints(applicantData[9]);
+        totalScore += calculateWorkExperiencePoints(applicantData[10]);
+        totalScore += calculateAgePoints(Integer.parseInt(applicantData[2]));
+        totalScore += calculateArrangedEmploymentPoints(applicantData[11]);
         totalScore += calculateAdaptabilityPoints(applicantData);
         return totalScore;
     }
 
     public static int calculateLanguagePoints(String[] applicantData) {
         int points = 0;
-        points += calculateSinglePoint(applicantData[INDEX_READING]);
-        points += calculateSinglePoint(applicantData[INDEX_WRITING]);
-        points += calculateSinglePoint(applicantData[INDEX_SPEAKING]);
-        points += calculateSinglePoint(applicantData[INDEX_LISTENING]);
-        if ("yes".equalsIgnoreCase(applicantData[INDEX_SPOUSE_LANGUAGE])) {
+        points += calculateSinglePoint(applicantData[4]);
+        points += calculateSinglePoint(applicantData[5]);
+        points += calculateSinglePoint(applicantData[6]);
+        points += calculateSinglePoint(applicantData[7]);
+        if ("yes".equalsIgnoreCase(applicantData[8])) {
             points += 4;
         }
         return points;
@@ -219,18 +188,20 @@ public class JustDoit {
     public static int calculateArrangedEmploymentPoints(String arrangedEmployment) {
         return "yes".equalsIgnoreCase(arrangedEmployment) ? 10 : 0;
     }
+
 	/* Calculates and awards points based on Adaptability requirements
 	 * Max score is 10
 	 */
     public static int calculateAdaptabilityPoints(String[] applicantData) {
         int score = 0;
-        if ("yes".equalsIgnoreCase(applicantData[INDEX_ADAPTABILITY_1])) score += 5;
-        if ("yes".equalsIgnoreCase(applicantData[INDEX_ADAPTABILITY_2])) score += 5;
-        if ("yes".equalsIgnoreCase(applicantData[INDEX_ADAPTABILITY_3])) score += 5;
-        if ("yes".equalsIgnoreCase(applicantData[INDEX_ADAPTABILITY_4])) score += 5;
-        if ("yes".equalsIgnoreCase(applicantData[INDEX_ADAPTABILITY_5])) score += 10;
-        if ("yes".equalsIgnoreCase(applicantData[INDEX_ADAPTABILITY_6])) score += 5;
-        if ("yes".equalsIgnoreCase(applicantData[INDEX_ADAPTABILITY_7])) score += 5;
+        if ("yes".equalsIgnoreCase(applicantData[12])) score += 5;
+        if ("yes".equalsIgnoreCase(applicantData[13])) score += 5;
+        if ("yes".equalsIgnoreCase(applicantData[14])) score += 5;
+        if ("yes".equalsIgnoreCase(applicantData[15])) score += 5;
+        if ("yes".equalsIgnoreCase(applicantData[16])) score += 10;
+        if ("yes".equalsIgnoreCase(applicantData[17])) score += 5;
+        if ("yes".equalsIgnoreCase(applicantData[18])) score += 5;
+        
         
         return Math.min(score, 10);
     }
